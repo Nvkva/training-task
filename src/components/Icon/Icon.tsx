f42ec6c5bc8063b8ca9c7b08755e1c988a5f590f
Diana Novikova <bogdanova.4.diana@yandex.ru>
2025-01-14 21:33:48 +0300

    Добавила компонент Icon и Input

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,6 +1,23 @@
+import clsx from 'clsx';
+import styles from './Icon.module.scss';
+import Icons from './icons.svg';
+
 export type IconProps = {
 	name: string;
 	size?: number;
 	color?: string;
 	className?: string;
 };
+
+export function Icon({ name, className, size = 24, color = 'inherit' }: IconProps) {
+	return (
+		<svg
+			className={clsx(styles.container, className)}
+			width={size}
+			height={size}
+			style={{ color }}
+		>
+			<use href={`${Icons}#${name}`} />
+		</svg>
+	);
+}
