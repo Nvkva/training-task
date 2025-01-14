1f0d280df596d2dd23d4ae869d3c8b5b57811226
Diana Novikova <bogdanova.4.diana@yandex.ru>
2025-01-14 21:32:51 +0300

    Добавила компонент Anchor

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -1,4 +1,5 @@
 import { AnchorHTMLAttributes, MouseEvent } from 'react';
+import { useAnchor, useLinkProps } from './AnchorContext';
 
 export type Link = string | ((event: MouseEvent<HTMLAnchorElement>) => void);
 
@@ -8,3 +9,13 @@ export type AnchorProps = Omit<
 > & {
 	link?: Link;
 };
+
+export function Anchor({ href, children, ...props }: AnchorProps) {
+	const LinkElement = useAnchor(href);
+	const propsLink = useLinkProps(href);
+	return (
+		<LinkElement {...props} {...propsLink}>
+			{children}
+		</LinkElement>
+	);
+}
