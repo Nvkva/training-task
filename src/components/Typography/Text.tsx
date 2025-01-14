88e602e9b058fc3b8d3696e30097bd1b4a67fbcf
Diana Novikova <bogdanova.4.diana@yandex.ru>
2025-01-14 22:48:31 +0300

    Добавила компонент Section

--- src/components/Typography/Text.tsx
+++ src/components/Typography/Text.tsx
@@ -1,33 +1,41 @@
-import clsx from "clsx";
+import clsx from 'clsx';
 import styles from './Text.module.scss';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-export type TranslationKey =     "demo.welcomeUser"|
-    "demo.logIn"|
-    "demo.logOut"|
-    "demo.signUp"|
-    "main.title"|
-    "main.description"|
-    "main.applyVolunteer"|
-    "main.volunteerMotivation"|
-    "main.applyFund"|
-    "main.fundMotivation";
+const translationKey = [
+	'demo.welcomeUser',
+	'demo.logIn',
+	'demo.logOut',
+	'demo.signUp',
+	'main.title',
+	'main.description',
+	'main.applyVolunteer',
+	'main.volunteerMotivation',
+	'main.applyFund',
+	'main.fundMotivation',
+] as const;
+export type TranslationKey = (typeof translationKey)[number];
+
+export const isTranslationKey = (x: any): x is TranslationKey => translationKey.includes(x);
+
 
 export type TextProps = {
 	className?: string;
-	value: TranslationKey;
+	value: TranslationKey | string;
 	Tag: keyof React.JSX.IntrinsicElements;
 };
 
 function withType(defaultTag: keyof React.JSX.IntrinsicElements) {
 	const { t } = useTranslation();
-	return function Text({ className, value, Tag = defaultTag}: TextProps) {
-		return <Tag className={clsx(styles.container, className)}>
-			{t(value)}
-		</Tag>;
-	}
+	return function Text({ className, value, Tag = defaultTag }: TextProps) {
+		return (
+			<Tag className={clsx(styles.container, className)}>
+				{isTranslationKey(value) ? t(value) : value}
+			</Tag>
+		);
+	};
 }
 
-export const Text = withType("span");
+export const Text = withType('span');
 
-export const Title = withType("h3");
+export const Title = withType('h3');
