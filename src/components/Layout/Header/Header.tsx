117f6489e68cc0728b04e271daf278c22208fd03
Diana Novikova <bogdanova.4.diana@yandex.ru>
2025-01-14 23:49:53 +0300

    Добавила компонент Header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,4 +1,8 @@
-import { ReactNode } from "react";
+import { FunctionComponent, ReactNode } from 'react';
+import clsx from 'clsx';
+
+import styles from './Header.module.scss';
+import { Anchor } from '@/components/Anchor/Anchor';
 
 export type MenuLink = {
 	label: string | ReactNode;
@@ -6,8 +10,37 @@ export type MenuLink = {
 };
 
 export type HeaderProps = {
-  className?: string;
+	className?: string;
 	menu: MenuLink[];
 	children?: ReactNode;
 	isSticky?: boolean;
+};
+
+export const Header: FunctionComponent<HeaderProps> = ({
+	className,
+	isSticky = true,
+	menu,
+	children,
+}: HeaderProps) => {
+	return (
+		<header
+			className={clsx(styles.header, className, {
+				[styles.sticky]: isSticky,
+			})}
+		>
+			<div className={styles.container}>
+				<Anchor href="/" className={styles.logo} />
+				<nav className={clsx(styles.menu)}>
+					<ul className={styles.menuContent}>
+						{menu.map(({ label, href }, index) => (
+							<li key={index} className={styles.link}>
+								<Anchor href={href}>{label}</Anchor>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<nav className={styles.controls}>{children}</nav>
+			</div>
+		</header>
+	);
 };
