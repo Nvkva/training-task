75ffdba3a6552a968978cca71695c0d08ba45db0
Diana Novikova <bogdanova.4.diana@yandex.ru>
2025-01-14 21:33:18 +0300

    Добавила компонент Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,9 @@
-import { ReactNode } from "react";
-import { ButtonTag, ButtonType, ButtonVariant } from "./types";
+import { ReactNode } from 'react';
+import { ButtonTag, ButtonType, ButtonVariant } from './types';
+import styles from './Button.module.scss';
+import { Anchor } from '../Anchor/Anchor';
+import clsx from 'clsx';
+import { Icon } from '@/ui';
 
 export type ButtonProps = {
 	type?: ButtonType;
@@ -12,3 +16,39 @@ export type ButtonProps = {
 	href?: string;
 	Tag?: ButtonTag;
 };
+
+function withTag(defaultTag: ButtonTag = 'button') {
+	return function Button({
+		type = ButtonType.Button,
+		variant = ButtonVariant.Primary,
+		disabled = false,
+		waiting = false,
+		onClick,
+		className,
+		children,
+		href,
+		Tag = defaultTag,
+	}: ButtonProps) {
+		const TagElement = Tag === 'a' ? Anchor : Tag;
+
+		return (
+			<TagElement
+				type={Tag === 'button' ? type : undefined}
+				href={Tag === 'a' ? href : undefined}
+				className={clsx(styles.button, styles[variant], className, {
+					[styles.disabled]: disabled,
+					[styles.waiting]: waiting,
+				})}
+				onClick={onClick}
+				disabled={disabled}
+			>
+				{waiting && <Icon name="loader" />}
+				{children}
+			</TagElement>
+		);
+	};
+}
+
+export const Button = withTag();
+
+export const ButtonLink = withTag('a');
